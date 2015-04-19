3453b341b47141b3de77815c5fceab4ec3599d30
Jim MacArthur <jim-git@mode7.co.uk>
2015-04-19 13:36:03 +0100

    Player sprite; increased tile size

--- loop.js
+++ loop.js
@@ -7,13 +7,13 @@ var SCREENHEIGHT = 480;
 var MODE_TITLE = 0;
 var MODE_PLAY  = 1;
 var MODE_WIN   = 2;
-var TILESIZE = 16;
+var TILESIZE = 32;
 var levels = new Array(16);
 var EVAPSTEP = 4; // Make this bigger to slow evaporation
 var PRESSURE = 16; // Water pressure
 
 flash = "";
-
+tileColours = [ "#000000", "#ffffff", "#0000cf" ];
 function Robot(sx,sy)
 {
     this.x = sx;
@@ -24,8 +24,8 @@ function Robot(sx,sy)
     this.miny = 0;
     this.dx = 4;
     this.dy = 0;
-    this.xsize = 16;
-    this.ysize = 16;
+    this.xsize = 32;
+    this.ysize = 32;
     this.colour = "#ff0000";
     this.health = 32;
 }
@@ -45,6 +45,8 @@ function Level()
     }
     this.robots = [];
     this.robots.push(new Robot(32,128))
+    this.exitX = 480-TILESIZE;
+    this.exitY = 0;
 }
 
 function Particle(x,y,xvel,yvel)
@@ -62,7 +64,8 @@ function fakeLevel()
 	currentLevel.map[x+14][9] = 1;
 	currentLevel.map[x+3][13] = 1;
 	currentLevel.map[x+7][12] = 1;
-	currentLevel.map[x*2][20] = 1;
+	currentLevel.map[x*2][13] = 1;
+	currentLevel.map[x+2][12] = 2;
     }
     currentLevel.map[2][12] = 1;
 }
@@ -140,7 +143,9 @@ function init()
 {
     mode = MODE_TITLE;
     playerImage = getImage("person");
+    gunImage = getImage("watergun");
     bottleImage = getImage("bottle");
+    robotImage = getImage("robot");
     springSound = new Audio("audio/boing.wav");
     makeTitleBitmaps();
     return true;
@@ -152,7 +157,7 @@ function draw() {
 	flash = "";
     }
     else {
-	ctx.fillStyle = "#00007f";
+	ctx.fillStyle = "#00003f";
     }
 
     ctx.fillRect(0, 0, SCREENWIDTH, SCREENHEIGHT);
@@ -167,19 +172,20 @@ function draw() {
 
    for(cx=0;cx<640/TILESIZE;cx++) {
 	for(cy=0;cy<480/TILESIZE;cy++) {
-	    if(currentLevel.map[cx][cy] == 1) {
+	    ctx.fillStyle = tileColours[currentLevel.map[cx][cy]];
+	    if(currentLevel.map[cx][cy] > 0) {
 		ctx.fillRect(cx*TILESIZE, cy*TILESIZE, TILESIZE, TILESIZE);
-		}
-	    if(currentLevel.water[cx][cy] > 0) {
-		w = currentLevel.water[cx][cy];
-		ctx.fillRect(cx*TILESIZE, cy*TILESIZE+TILESIZE-w, TILESIZE, w);
 		}
 	    }
 	}
 
     ctx.drawImage(playerImage, px, py);
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(px+TILESIZE/2 + aimDirection * Math.cos(aimAngle)*32, py+TILESIZE/2 + Math.sin(aimAngle)*32, 4,4);
+    ctx.save();
+    ctx.translate(px+TILESIZE/2,py+TILESIZE);
+    ctx.rotate(aimAngle*aimDirection);
+    ctx.scale(0.5*aimDirection,0.5);
+    ctx.drawImage(gunImage, -32,-10);
+    ctx.restore();
 
     ctx.fillStyle = "#7f7fff";
 
@@ -201,8 +207,8 @@ function draw() {
 
     for(i=0;i<currentLevel.robots.length;i++) {
 	r = currentLevel.robots[i];
+	ctx.drawImage(robotImage, r.x, r.y);
 	ctx.fillStyle = r.colour;
-	ctx.fillRect(r.x, r.y, r.xsize,r.ysize);
     }
 
     for(i=0;i<particles.length;i++) {
@@ -212,9 +218,13 @@ function draw() {
 	ctx.fillRect(p.x, p.y, 4, 4);
     }
 
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(currentLevel.exitX, currentLevel.exitY, 64,64);
+
     if(mode == MODE_WIN) {
 	ctx.drawImage(winBitmap, 0, 0);
     }
+
 }
 
 
@@ -222,7 +232,7 @@ function risefall(dy)
 {
     x1 = Math.floor(px/TILESIZE);
     x2 = Math.floor((px+TILESIZE-1)/TILESIZE);
-    gridy = Math.floor(py/TILESIZE)+dy;
+    gridy = Math.floor(py/TILESIZE)+2*dy;
     if(py % TILESIZE == 0) {
 	
 	ground1 = currentLevel.map[x1][gridy]
@@ -239,10 +249,12 @@ function risefall(dy)
 }
 
 function waterLand(gx,gy) {
-    currentLevel.water[gx][gy] += 1;
-    if(currentLevel.water[gx][gy] >= 16) {
-	currentLevel.map[gx][gy] = 1;
-    }
+    if(currentLevel.map[gx][gy] == 0) {
+	currentLevel.water[gx][gy] += 1;
+	if(currentLevel.water[gx][gy] >= 16) {
+	    currentLevel.map[gx][gy] = 1;
+	}
+	}
     addDrip(gx,gy);
 }
 
@@ -370,6 +382,16 @@ function action()
 	}
     }
     particles = newParticles;
+    if(px >= currentLevel.exitX && px < currentLevel.exitX+64 && py >= currentLevel.exitY && py < currentLevel.exitY+64) {
+	resetGame();
+    }
+
+    // Tile effects
+    gx = Math.floor(px/TILESIZE);
+    gy = Math.floor(py/TILESIZE)+1;
+    if(currentLevel.map[gx][gy] == 2) {
+	waterLevel = 128;
+    }
 }
 
 
@@ -380,9 +402,10 @@ function moveX(dx)
 	gx = Math.floor(px/TILESIZE);
 	gy = Math.floor(py/TILESIZE);
 	gy2 = Math.floor((py+TILESIZE-1)/TILESIZE);
+	gy3 = Math.floor((py+TILESIZE*2-1)/TILESIZE);
 	if(dx < 0) gx -= 1;
 	if(dx > 0) gx += 1;
-	if(currentLevel.map[gx][gy] == 1 || currentLevel.map[gx][gy2] == 1) {
+	if(currentLevel.map[gx][gy] == 1 || currentLevel.map[gx][gy2] == 1 || currentLevel.map[gx][gy3] == 1) {
 	    return false;
 	}
      }
@@ -393,7 +416,7 @@ function moveX(dx)
 function addWater()
 {
     if(waterLevel > 0) {
-	waterParticles.push([px+TILESIZE/2, py+TILESIZE/2, aimDirection * Math.cos(aimAngle) * PRESSURE, Math.sin(aimAngle) * PRESSURE]);
+	waterParticles.push([px+TILESIZE/2, py+TILESIZE, aimDirection * Math.cos(aimAngle) * PRESSURE, Math.sin(aimAngle) * PRESSURE]);
 	waterLevel -= 1;
     }
 }
@@ -401,10 +424,11 @@ function addWater()
 function processKeys() {
     if(keysDown[37] || keysDown[65]) moveX(-4);
     if(keysDown[39] || keysDown[68]) moveX(4);
-    if(keysDown[32] & grounded) { yvel = -6; grounded = false; }
+    if(keysDown[32] & grounded) { yvel = -10; grounded = false; }
     if(keysDown[40] && aimAngle < Math.PI / 2) aimAngle += 0.1;
     if(keysDown[38] && aimAngle > -Math.PI / 2) aimAngle -= 0.1;
     if(keysDown[87]) addWater();
+    if(keysDown[88]) waterLevel = 128; // Cheat code
 }
 
 function drawRepeat() {
